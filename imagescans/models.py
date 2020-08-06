1144a5195c5c22fbe459b8ebea1ecbec1f7dd8b8
akashkrshukla <akashshukla778@gmail.com>
2020-08-06 12:41:56 +0530

    null value restriction

--- imagescans/models.py
+++ imagescans/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ImageScans(models.Model):
-    image = models.FileField(upload_to='ImageScans/', null=True, blank=True)
+    image = models.FileField(upload_to='ImageScans/', null=False, blank=False)
 
     def __str__(self):
         return str(self.name)
