ac602c6cd69f951e34ff4f11a8a5ef1e468c6a0f
akashkrshukla <akashshukla778@gmail.com>
2020-08-17 00:08:45 +0530

    upto model relevence with the image set & binding

--- imagescans/models.py
+++ imagescans/models.py
@@ -1,13 +1,33 @@
 from django.db import models
 import os
+import os
+
+
 # Create your models here.
 
 class Config():
     UPLOAD_IMAGE_PATH = "/home/akash/projects/django/mriScan/run/media/ImageScans/"
+    ANALYZED_IMAGE_PATH = "/home/akash/projects/django/mriScan/run/media/ImageResults/"
+
+
+
 class ImageScans(models.Model):
-    image = models.FileField(upload_to= 'ImageScans', null=False, blank=False)
-    analyzed_image = models.FileField(upload_to= 'ImageResults', null=True, blank= True)
-    user_id = models.CharField(max_length=10,)
+    user_id = models.CharField(max_length=10, )
+    image = models.FileField(upload_to='ImageScans' , null=False, blank=False)
+    analyzed_image = models.FileField(upload_to='ImageResults', null=True, blank=True)
+    
+    def save(self):
+        for field in self._meta.fields:
+            if field.name == 'image':
+                field.upload_to = 'ImageScans/%s' % self.user_id
+        super(ImageScans, self).save()
+
+    def saveResults(self):
+        for field in self._meta.fields:
+            if field.name == 'analyzed_image':
+                field.upload_to = 'ImageResults/%s' % self.user_id
+        super(ImageScans, self).save()
+
+
     def __str__(self):
         return str(self.name)
-    
--- imagescans/views.py
+++ imagescans/views.py
@@ -5,56 +5,61 @@ from .services import Neuralnetwork
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import json
 from random import randint
+import os
+
 
 def uploadImage(request):
     res = {}
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
         if user_id:
+            try:
+                if os.path.exists(os.path.join(Config.UPLOAD_IMAGE_PATH, (user_id + "/"))):
+                    print('Exists')
+                else:
+                    print('Creating')
+                    os.mkdir(os.path.join(Config.UPLOAD_IMAGE_PATH, (user_id + "/")))
+                    
+            except NotADirectoryError:
+                print('Error')
             data = request.FILES.get('image')
             rec_n = ImageScans()
             rec_n.image = data
             rec_n.user_id = user_id
             rec_n.save()
-            evaluateImage(Config.UPLOAD_IMAGE_PATH, user_id)
+            analyzedImage = evaluateImage(Config.UPLOAD_IMAGE_PATH, user_id)
+            rec_n.analyzed_image = data
+            rec_n.saveResults()
             res['status'] = 1
-            # try:
-            #     data = request.FILES.get('image')
-            #     rec_n = ImageScans()
-            #     rec_n.image = data
-            #     rec_n.save()
-            #     evaluateImage(rec_n.image)
-            #     res['status'] = 1
-            # except:
-            #     res['status'] = -1
             return JsonResponse(res)
         else:
             res['status'] = 0
             return JsonResponse(res)
 
 
-def evaluateImage(base_directory, user_id ):
-    test_dir = os.path.join(base_directory, user_id )
+def evaluateImage(base_directory, user_id):
+    test_dir = os.path.join(base_directory)
 
     testAug = ImageDataGenerator(
-    rescale=1 / 255.0,
-    rotation_range=20,
-    zoom_range=0.05,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.05,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode="nearest")
+        rescale=1 / 255.0,
+        rotation_range=20,
+        zoom_range=0.05,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.05,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode="nearest")
 
     testGen = testAug.flow_from_directory(
-    test_dir,
-    class_mode="categorical",
-    target_size=(48, 48),
-    color_mode="rgb",
-    shuffle=False,
-    batch_size=1)
+        test_dir,
+        class_mode="categorical",
+        target_size=(48, 48),
+        color_mode="rgb",
+        shuffle=False,
+        batch_size=1)
 
     model = Neuralnetwork.loadModel()
+    #return #return analysedImage now.
     # model.predict(x= testGen, steps=(totalTest // 1) + 1)
     # model.summary()  # This can be used to show a detailed summary of model.
